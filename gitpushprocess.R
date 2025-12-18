4b0238d54e15340158d2203ceb72512a06a9958c
Sarah L. Kiefer <sarah_kiefer@brown.edu>
2025-12-18 12:08:36 -0500

    commit

---
+++ gitpushprocess.R
@@ -0,0 +1,6 @@
+#to push to website, copy and paste each line individually into TERMINAL
+git remote add origin https://slkiefer.github.io/ 
+git branch -M main
+git add --all
+git commit -m "commit"
+git push origin main
